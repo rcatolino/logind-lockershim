6a85745cb9144ecf91420d4256df852d943bc062
rca <raphael.catolino@gmail.com>
2018-04-15 09:59:12 +0200

    Get locker app from command line args

--- llsd.py
+++ llsd.py
@@ -3,6 +3,8 @@
 import os
 import dbus
 import subprocess
+import sys
+
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
 
@@ -18,21 +20,22 @@ class LogindManagerProxy:
         dbobj = bus.get_object(LogindManagerProxy.name, LogindManagerProxy.path)
         self.dbif = dbus.Interface(dbobj, dbus_interface=LogindManagerProxy.interface)
 
-    def get_user_session_proxy(self, session_id):
+    def get_user_session_proxy(self, session_id, locker_args):
         session_path = self.dbif.GetSession(session_id)
-        return LogindSessionProxy(self.bus, session_path)
+        return LogindSessionProxy(self.bus, session_path, locker_args)
 
 class LogindSessionProxy:
     name = "org.freedesktop.login1"
     interface = "org.freedesktop.login1.Session"
     properties = "org.freedesktop.DBus.Properties"
 
-    def __init__(self, bus, path):
+    def __init__(self, bus, path, locker_args):
         dbobj = bus.get_object(LogindSessionProxy.name, path)
         self.dbif = dbus.Interface(dbobj, dbus_interface=LogindSessionProxy.interface)
         self.props = dbus.Interface(dbobj, dbus_interface=LogindSessionProxy.properties)
         self.dbif.connect_to_signal("Lock", lambda: self.on_lock())
         self.dbif.connect_to_signal("Unlock", lambda: self.on_unlock())
+        self.locker_args = locker_args
         self.locker = None
 
     def is_locked(self):
@@ -44,7 +47,7 @@ class LogindSessionProxy:
             print("Already locked, ignoring lock request")
             return
 
-        self.locker = subprocess.Popen(["/usr/bin/swaylock", "-i", "/boot/background.png", "--scaling", "stretch"])
+        self.locker = subprocess.Popen(self.locker_args)
 
     def on_unlock(self):
         print("Unlock signal")
@@ -59,9 +62,15 @@ class LogindSessionProxy:
     def get_prop(self, pname):
         return self.props.Get(LogindSessionProxy.interface, pname)
 
+locker_args = sys.argv[1:]
+if len(locker_args) == 0 or locker_args[0] == "-h" or locker_args[0] == "--help":
+    print("Usage : {} <locker exec> <locker args>".format(sys.argv[0]))
+    print("Example : {} /usr/bin/swaylock -i picture.png --scaling stretch".format(sys.argv[0]))
+    exit(0)
+
 system_bus = dbus.SystemBus()
 manager = LogindManagerProxy(system_bus)
-proxy = manager.get_user_session_proxy(os.environ['XDG_SESSION_ID'])
+proxy = manager.get_user_session_proxy(os.environ['XDG_SESSION_ID'], locker_args)
 
 print("{}\t{}\t{}\t{}\t{}".format(proxy.get_prop("Id"), proxy.get_prop("Name"),
     proxy.get_prop("TTY"), proxy.get_prop("Type"), proxy.get_prop("LockedHint")))
